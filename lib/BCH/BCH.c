f62616d799f681938f4b1ececc81272a1bc96a79
Alex (UnHold) <alex@hold.re>
2019-11-06 15:16:49 +0100

    added the files that will be used to generate and check the BCH code

---
+++ lib/BCH/BCH.c
@@ -0,0 +1,9 @@
+
+#include "stdbool.h"
+#include "BCH.h"
+
+bool checkBCH(long message){
+
+    //return just a placeholder for now
+    return true;
+}
---
+++ lib/BCH/BCH.h
@@ -0,0 +1,15 @@
+/*
+Lib for creating BCH codes and checking BCH codes
+*/
+
+#ifndef BCH_h
+#define BCH_h
+#endif
+
+class BCH{
+    //public functions
+    public:
+
+    //private functions
+    private:
+};
