880ea27ae08b992c433ac40e6d17d8a0e8637af2
Alex (UnHold) <alex@hold.re>
2019-11-08 11:47:45 +0100

    added some static vars for pocsag and improved the getBinaryLength function

--- lib/BCH/BCH.cpp
+++ lib/BCH/BCH.cpp
@@ -3,14 +3,24 @@
 #include "math.h"
 #include "BCH.h"
 
+//0b11101101001 = generator polynom used by pocsag: x^10 + x^9 + x^8 + x^6 + x^5 + x^3 + 1
+#define POCSAG_GENERATOR_POLYNOM 0b11101101001
+#define POCSAG_DATA_LENGTH 21
+#define POCSAG_CODE_LENGTH 31
 
-//get the binary length of an number from type long
+//get the binary length of an number from type int
 int BCH::getBinaryLength(int number){
-    return floor(log2(number)) + 1;
+    //uses the long function
+    return getBinaryLength((long) number);
 }
 
 //get the binary length of an number from type long
 int BCH::getBinaryLength(long number){
+
+    //special case 0 -> would otherwise return -2147483648
+    if(number == 0){
+        return 0;
+    }
     return (int) (floor(log2(number)) + 1);
 }
 
