da0ecb23832528c674638d8a35e3c8f340e3b09c
Alex (UnHold) <alex@hold.re>
2020-02-05 13:51:58 +0100

    added SPI wrapper class for better function names

---
+++ lib/SPI/SPIc.cpp
@@ -0,0 +1,84 @@
+#include "SPIc.h"
+
+/*
+Missing implementation (not nessasary at the moment): 
+setHwCs(); setFrequency(); setClockDivider(); 
+getClockDivider(); setDataMode(); setBitOrder();
+
+Missing implementation (should be implemented and functionality documented)
+transferBits();  writePixels();  writePattern();
+INFO: transferBits is kind of broken (test signal before implementing (always 8 useless clk cycles))
+*/
+
+/*
+Mostly a warpper class, used to easily implement special functions
+heavely based on:
+https://github.com/espressif/arduino-esp32/blob/master/libraries/SPI/src/SPI.cpp
+*/
+
+SPIClass SPIcSPI(HSPI);
+
+//uses hspi 
+#define SPIc_SPI_CLK 14
+#define SPIc_SPI_MISO 12
+#define SPIc_SPI_MOSI 13
+
+#define SPIc_SPI_SS_1 15
+
+void SPIc::setup(){
+    SPIcSPI.begin(SPIc_SPI_CLK, SPIc_SPI_MISO, SPIc_SPI_MOSI, -1);
+}
+
+void SPIc::close(){
+    SPIcSPI.end();
+}
+
+void SPIc::beginTransaction(uint32_t maxSpeed, uint8_t bitOrder, uint8_t spiMode){
+    SPIcSPI.beginTransaction(SPISettings(maxSpeed, bitOrder, spiMode));
+}
+
+void SPIc::beginTransaction(SPISettings settings){
+    SPIcSPI.beginTransaction(settings);
+}
+
+void SPIc::endTransaction(){
+    SPIcSPI.endTransaction();
+}
+
+
+//for writing (sending data to the slave)
+void SPIc::write8(uint8_t data){
+    SPIcSPI.write(data);
+}
+
+void SPIc::write16(uint16_t data){
+    SPIcSPI.write16(data);
+}
+
+void SPIc::write32(uint32_t data){
+    SPIcSPI.write32(data);
+}
+
+//for writing array 
+void SPIc::writeArray8(uint8_t * data, uint32_t size){
+    SPIcSPI.writeBytes(data, size);
+}
+
+//for read writing (sending data to the slave and receiving a response)
+uint8_t SPIc::transfer8(uint8_t data){
+    return SPIcSPI.transfer(data);
+}
+
+uint16_t SPIc::transfer16(uint16_t data){
+    return SPIcSPI.transfer16(data);
+}
+
+uint32_t SPIc::transfer32(uint32_t data){
+    return SPIcSPI.transfer32(data);
+}
+
+uint8_t * SPIc::transferArray8(uint8_t * data, uint32_t size){
+    uint8_t * result = nullptr;
+    SPIcSPI.transferBytes(data, result, size);
+    return result;
+}
---
+++ lib/SPI/SPIc.h
@@ -0,0 +1,36 @@
+/*
+Lib for creating and managing pocsag messages
+*/
+
+#ifndef SPIc_h
+#define SPIc_h
+
+#include "SPI.h"
+
+class SPIc{
+
+    
+
+    public:
+       void setup();
+       void close();
+       void beginTransaction(uint32_t maxSpeed, uint8_t bitOrder, uint8_t spiMode);
+       void beginTransaction(SPISettings settings);
+       void endTransaction();
+       //write
+       void write8(uint8_t data);
+       void write16(uint16_t data);
+       void write32(uint32_t data);
+       void writeArray8(uint8_t * data, uint32_t size);
+       //transfer (write read)
+       uint8_t transfer8(uint8_t data);
+       uint16_t transfer16(uint16_t data);
+       uint32_t transfer32(uint32_t data);
+       uint8_t * transferArray8(uint8_t * data, uint32_t size);
+    private:
+        
+
+};
+
+
+#endif
